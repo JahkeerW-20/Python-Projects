a298aa3c0a217be990f739416fe50f6e7e26991a
JahkeerW-20 <123904353+JahkeerW-20@users.noreply.github.com>
2023-01-29 16:30:50 -0500

    Add files via upload
    Madlib: 1

---
+++ madlibs.py
@@ -0,0 +1,21 @@
+# string concatenation (aka how to put strings together)
+
+# we want to create a madlib that allows you to tell your iwn story!!
+adj = input("Adjective: ")
+adj2 = input("Adjective: ")
+adj3 = input("Adjective: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+noun4 = input("Noun: ")
+
+
+
+madlib = f"Let us enjoy reading this {adj} Tale of {adj2} Teacher . A school Teacher used to take a {adj3} nap every {noun2}. \
+When his students asked him why he did so, he said that he went to dreamland to meet ancient {noun4}.\
+One extremely hot day some of the {noun3} fell asleep in the afternoon. When the school-Teacher scolded them, they said, \
+'We went to meet the {noun4} in dreamland.'\
+'What did they say?' \
+demanded the Teacher.\
+'We asked them if a school-Teacher came there every {noun2}, but they said they had seen no such person."
+
+print(madlib)

b59cdda65f9cc37d32a1a33d81a4052f20d94bec
JahkeerW-20 <123904353+JahkeerW-20@users.noreply.github.com>
2023-02-01 00:17:40 -0500

    Add files via upload
    2nd iteration of a password checker I started with still looking to make it  a  little more.

---
+++ passwordchkr2.py
@@ -0,0 +1,32 @@
+import re
+
+# module of regular expression
+password = input("Password:   ")  # user input for checking password
+flag = 0
+while True:
+    if len(password) <= 8:
+        flag = -1
+        break
+    elif not re.search("[a-z]", password):
+        flag = -1
+        break
+    elif not re.search("[A-Z]", password):
+        flag = -1
+        break
+    elif not re.search("[0-9]", password):
+        flag = -1
+        break
+    elif not re.search("[_@$]", password):
+        flag = -1
+        break
+    elif re.search("\s", password):
+        flag = -1
+        break
+    else:
+        flag = 0
+        print("Valid Password")
+        break
+if flag == -1:
+    print("Not a Valid Password")
+
+# still working on this one, might add a few things, tweak some things here and there
